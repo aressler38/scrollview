e6bdd75681893a7253f604f3752e853e0f5b725a
alexander <alexander.ressler@gmail.com>
2015-11-19 01:39:38 +0000

    scroll forever, loop and use modular arithmetic for recycling

--- src/js/scrollview.js
+++ src/js/scrollview.js
@@ -1,26 +1,66 @@
 'use strict';
 
+
+
 class ScrollView {
 
 	constructor() {
+		var self = this;
 		var containerEl =  document.createElement('div');
 		containerEl.classList.add('scroll-view-element');
 		this.containerTemplate = null;
+		this.containerTemplateHeight = null;
 
-		this.setContainerTemplate(this.containerTemplate);
+		this.setContainerTemplate(containerEl);
 
 		this.el = document.createElement('div');
 		this.el.classList.add('scroll-view');
 
 		this.dataset = [ ];
+		this.containers = [ ];
 
-		//this.el.addEventListener('touchstart', 
+		this.y0 = null;
 		
+
+		var _isScrolling = false;
+
+		this.el.addEventListener('touchstart', function (e) {
+			self.y0 = e.touches[0].clientY;
+			_isScrolling = true;
+		});
+		this.el.addEventListener('touchmove', function (e) {
+			var y0, y;
+			if ( _isScrolling ) {
+				y0 = self.y0;
+				y = e.touches[0].clientY;
+				var dy = y - y0;
+				var dyMod = dy % self.containerTemplateHeight;
+				var container;
+				if ( dy > self.containerTemplateHeight ) {
+					container = self.el.firstChild; 
+					self.el.removeChild( container );
+					self.el.appendChild( container );
+				} else if ( dy < self.containerTemplateHeight ) {
+					container = self.el.lastChild; 
+					self.el.removeChild( container );
+					self.el.insertBefore(container, self.el.firstChild);
+				}
+				for ( var i=0; i<self.containers.length; ++i ) {
+					self.containers[i].style.transform = 'translate3d(0,'+dyMod+'px,0)';
+				}
+			}
+		});
+		this.el.addEventListener('touchend', function (e) {
+			self.y0 = null;
+			_isScrolling = false;
+		});
+
+
 		Object.defineProperty(this, 'template', {
 			get : function () { return this.containerTemplate && this.containerTemplate.cloneNode(true); }
 		});
 
-		
+
 	}
 
 
@@ -28,31 +68,50 @@ class ScrollView {
 		this.containerTemplate = element.cloneNode(true);
 	}
 
+	appendContainer (data) {
+		var container = this.template;
+		container.innerHTML = data;
+		this.el.appendChild(container);
+		this.containers.push(container);
+		//TODO: render data on template
+
+	}
+
 	createContainers () {
-		clear();
+		this.clear();
 		var rect = this.el.getBoundingClientRect();
-		var scrollViewHeight = Math.ceil( rect.bottom - rect.top );
+		var computedStyle = getComputedStyle( this.el );
+		var scrollViewPadding = parseInt( computedStyle.padding );
+		var borderHeight = parseInt( computedStyle.borderBottomWidth ) + parseInt( computedStyle.borderTopWidth );
+		var scrollViewHeight = Math.ceil( rect.bottom - rect.top ) - scrollViewPadding - borderHeight;
 
 		this.el.appendChild(this.template);
 		var liveTemplate = this.el.querySelector(':first-child');
 
 		rect = liveTemplate.getBoundingClientRect();
 		var dy = Math.ceil( rect.bottom - rect.top );
+		this.containerTemplateHeight = dy;
 
+
+		this.clear();
 		// if y*dy == scrollViewHeight, then we need AT LEAST y sub templates to fill the view.
 
+		var y = Math.ceil( scrollViewHeight / dy );
 
-		
-
-
-
-
+		var bufferRoom = 3; // TODO: testing extra containers
+		for ( var i=0; i<y + bufferRoom; ++i ) {
+			this.appendContainer(''+i);
+		}
 
 	}
+
 
 	clear () {
 		while ( this.el.lastChild ) {
 			this.el.removeChild(this.el.lastChild);
+		}
+		while ( this.containers.length ) {
+			this.containers.pop();
 		}
 	}
 
@@ -74,4 +133,6 @@ class ScrollView {
 
 
 }
+
+module.exports = ScrollView;
 
--- test/scrollviewtest.js
+++ test/scrollviewtest.js
@@ -1,11 +1,19 @@
-var ScrollView = require('./scrollview');
+var ScrollView = require('../src/js/scrollview');
 
+if ( document.readyState === 'complete' || document.readyState === 'interactive' ) {
+	main()
+} else {
+	document.addEventListener('DOMContentLoaded', main);
+}
 
-
-(function main () {
+function main () {
 	
 	var scrollview = new ScrollView();
 
 	document.body.appendChild(scrollview.el);
 
-})();
+	scrollview.createContainers();
+
+	window.sv = scrollview;
+
+}
